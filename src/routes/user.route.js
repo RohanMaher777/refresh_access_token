2ca46c3567665167a7b6b88348ef33330bb6cca2
Your Name <rohanmaher97@gmail.com>
2023-11-21 09:22:17 +0530

    adding comments to the routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const {createUser} = require("../controller/user.registeration")
 const {generate_access_token} = require("../controller/generateAccessToken")
 
+//route for new user registration 
 router.post("/registeration", createUser )
 
+// route for generating the access_token using refresh_token
 router.get("/accesstoken", generate_access_token)
 
 
